e06a68def9ca6b5694555c5ca05f7202c47489ea
vellkunio <vellkunio>
2021-02-26 19:12:15 -0500

    basic implementing of db

---
+++ functions/index.js
@@ -0,0 +1,42 @@
+const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+const express = require('express');
+const app = express();
+
+app.get('/screams', (req, res) => {
+    admin.firestore().collection('screams').get()
+    .then(data => {
+        let screams = [];
+        data.forEach(doc => {
+            screams.push(doc.data());
+        });
+        return res.json(screams);
+    })
+    .catch(err => console.error(err));
+})
+
+app.post('/scream', (req, res) => {
+        const newScream = {
+            body: req.body.body,
+            userHandle: req.body.userHandle,
+            createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        };
+
+        admin
+            .firestore()
+            .collection('screams')
+            .add(newScream)
+            .then(doc => {
+                res.json({ message: `document ${doc.id} created successfully`});
+            })
+            .catch(err => {
+                res.status(500).json({ error: 'something went wrong'});
+                console.error(err);
+            });
+        
+    });
+
+exports.api = functions.https.onRequest(app);
