c1f989037fa6cfcfccdce43fc1138017dcd950a3
vellkunio <vellkunio>
2021-03-03 17:25:17 -0500

    Add and Get User Profile Details

--- functions/dbschema.js
+++ functions/dbschema.js
@@ -1,4 +1,16 @@
 let db ={
+    users: [
+        {
+            userId: 'dh23ggj5h32g543j5gf43',
+            email: 'user@gmail.com',
+            handle: 'user',
+            createdAt: '2011-10-05T14:48:00.000Z',
+            imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+            bio: 'Hello, my name is user, nice to meet you',
+            website: 'https://user.com',
+            location: 'London, ON'
+        }
+    ],
     screams: [
         {
             userHandle: 'user',
@@ -8,4 +20,4 @@ let db ={
             commentCount: 2
         }
     ]
-}
+};
---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyD2ljVznIDEXAHrAjPPssai8QYR1n8vI_0",
+    authDomain: "veddit-f1794.firebaseapp.com",
+    databaseURL: "https://veddit-f1794-default-rtdb.firebaseio.com",
+    projectId: "veddit-f1794",
+    storageBucket: "veddit-f1794.appspot.com",
+    messagingSenderId: "112041068143",
+    appId: "1:112041068143:web:7784af1ba2c67488436f9a",
+    measurementId: "G-Q7CJL77NJ3"
+  };
